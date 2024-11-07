1da2c474719b5fc8ca9be1c2f67b419ab704969b
Vivrz <163974647+Vivrz@users.noreply.github.com>
2024-11-07 19:54:54 +0530

    Lowest.java solved on the codeforces

---
+++ Lowest.java
@@ -0,0 +1,17 @@
+package Codeforces;
+import java.util.Scanner;
+public class Lowest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int f = sc.nextInt();
+        int s = sc.nextInt();
+        int t = sc.nextInt();
+        for(int i = 1 ;i <= f*s*t;i++){
+            if(i % f == 0 && i % s == 0 && i % t == 0 ){
+                System.out.println(i);
+                break;
+            }
+        }
+        sc.close();
+    }   
+}
