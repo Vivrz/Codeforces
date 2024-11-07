4f5f2b66a566bf936b07cdb1225f352064328d66
Vivrz <163974647+Vivrz@users.noreply.github.com>
2024-11-07 10:55:42 +0530

    Wrong_Subtraction.java solved on the codeforces

---
+++ Wrong_Substraction.java
@@ -0,0 +1,21 @@
+package Codeforces;
+import java.util.Scanner;
+public class Wrong_Substraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int i = 0;
+        while(i < k){
+            if(n%10 != 0){
+                n -= 1;
+            }
+            else if(n%10 == 0){
+                n/=10;
+            }
+            i++;
+        }
+        System.out.println(n);
+        sc.close();
+    }
+}
