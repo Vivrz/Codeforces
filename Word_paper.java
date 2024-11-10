9b6e340b6cc5a6cbddd6532aac78cdf4d8561317
Vivrz <163974647+Vivrz@users.noreply.github.com>
2024-11-10 21:13:04 +0530

    word on the paper solved on the codeforces

---
+++ Word_paper.java
@@ -0,0 +1,24 @@
+package Codeforces;
+import java.util.Scanner;
+public class Word_paper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0 ;i < n ;i++){
+            String l = "";
+            String r = "";
+            for(int k = 0 ;k < 8 ;k++){
+                String h = sc.nextLine();
+                l = l + h;
+            }
+            for(char e : l.toCharArray()){
+                if(e != '.'){
+                    r = r + e;
+                }
+            }
+            System.out.println(r);
+        }
+        sc.close();
+    }
+}
