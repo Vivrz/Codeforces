8a06f3af249d0e3e24cc89f83704746ee9a504de
Vivrz <163974647+Vivrz@users.noreply.github.com>
2024-11-13 23:54:31 +0530

    updated

--- Word_paper.java
+++ Word_paper.java
@@ -4,7 +4,7 @@ public class Word_paper {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
+        System.out.println("\n");
         for(int i = 0 ;i < n ;i++){
             String l = "";
             String r = "";
